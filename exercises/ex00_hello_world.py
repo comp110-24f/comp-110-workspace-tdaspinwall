4450c4d5c4cb0b4d9259ece104c1d1fe41d47fd6
tdaspinwall <theda@unc.edu>
2024-08-23 14:58:15 -0400

    Finished first exercise.

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in comp 110!!"""
+
+__author__ = "730751255"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function Definition."""
+    return "Hello, " + name + "!"
